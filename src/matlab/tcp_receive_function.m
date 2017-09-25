dce3f71295599b9961af109a5204133b67f48aa0
Robin Roche <robin.roche@gmail.com>
2017-09-25 17:01:36 +0200

    Update tcp_receive_function.m
    Included improvement suggestions from Sifakis Nikolaos

--- src/matlab/tcp_receive_function.m
+++ src/matlab/tcp_receive_function.m
@@ -1,16 +1,21 @@
 function y = tcp_receive_function(t)
-
+% Thanks to Sifakis Nikolaos for imrovement suggestions 
+ 
 message = '';
 content = '';
-
-% Receive lines of data from server 
+ 
+% Implement a waiting process
+while t.BytesAvailable == 0
+    pause(1)
+end
+% Receive lines of data from server
 while (get(t, 'BytesAvailable') > 0) 
-DataReceived = fread(t,t.BytesAvailable); 
+DataReceived = fread(t,t.BytesAvailable);
 message = strcat(message,DataReceived');
 end
-
+ 
 if(size(message)>0)
     content = textscan(message, '%*s %s %*s', 'delimiter', '"');    
 end
-
+ 
 y = content;
